bf7195690567bab0c3bd95ac2e470d2fb02d3380
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-22 12:43:52 -0700

    basic game feel for main menu - grow and srhink for button, basic sound design, and background ambience

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private float waitTime = 0.5f;
+    
     public void LoadGame()
     {
+        StartCoroutine(WaitForTransition(waitTime));
+    }
+
+    private IEnumerator WaitForTransition(float time)
+    {
+        yield return new WaitForSeconds(time);
         SceneLoader.LoadNext();
     }
 }
