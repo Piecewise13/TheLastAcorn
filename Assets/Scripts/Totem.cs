ee949a485fb213c3e608e88fb6242941043d9d19
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-23 10:19:53 -0700

    audio control throughout scenes

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -1,4 +1,4 @@
-using System.Collections; 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,22 +10,32 @@ public class Totem : MonoBehaviour
     [Header("Feedback")]
     [SerializeField] private ParticleSystem confetti;
     [SerializeField] private GrowAndShrink growAndShrink;
-    [SerializeField] private AudioPlayer audioPlayer;
+    [SerializeField] private AudioPlayer sfxPlayer;
 
+    [Header("Disable Sources")]
+    [SerializeField] private AudioPlayer ambiencePlayer;
+    [SerializeField] private AudioPlayer musicPlayer;
+
+    const float FADE_TIME = 1.5f;
     const float DELAY_BEFORE_LOAD = 3f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+
+        GetComponent<Collider2D>().enabled = false;
         StartCoroutine(TriggerFeedbackAndLoad());
     }
 
     IEnumerator TriggerFeedbackAndLoad()
     {
+        if (ambiencePlayer) ambiencePlayer.FadeOut(FADE_TIME);
+        if (musicPlayer)    musicPlayer.FadeOut(FADE_TIME);
+
         if (confetti) confetti.Play();
         if (growAndShrink) growAndShrink.Grow();
-        if (audioPlayer) audioPlayer.Play();
-        
+        if (sfxPlayer) sfxPlayer.Play();
+
         yield return new WaitForSeconds(DELAY_BEFORE_LOAD);
         SceneManager.LoadScene(nextSceneName);
     }
