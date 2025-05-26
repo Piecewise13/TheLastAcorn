52adda5d56ea417dee0f195c21f57777ec87d0c1
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-26 12:27:54 -0700

    Update AudioPlayer.cs

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -13,7 +13,7 @@ public class AudioPlayer : MonoBehaviour
     public AudioClip audioClip;
 
     [Range(0f, 1f)] public float volume = 1f;
-    [Range(0.5f, 3f)] public float pitch = 1f;
+    [Range(-3f, 3f)] public float pitch = 1f;
     public bool loop = false;
 
     //  Randomisation
