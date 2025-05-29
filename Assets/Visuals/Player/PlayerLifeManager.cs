f3c44343ea39eebc39773ad3e3a5765e31eeba9d
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-28 22:06:27 -0700

    Hurt SFXs Implemented

--- Assets/Visuals/Player/PlayerLifeManager.cs
+++ Assets/Visuals/Player/PlayerLifeManager.cs
@@ -6,21 +6,23 @@ public class PlayerLifeManager : MonoBehaviour
 {
 
     [SerializeField] private int maxLives = 3;
-    private int currentLives;
 
     [SerializeField] private Sprite[] lifeIcons;
 
     [SerializeField] private Image lifeUI;
 
-    void Start()
-    {
-        currentLives = maxLives;
-    }
+    [Header("Feedback")] 
+    [SerializeField] private AudioPlayer hurtSfx;
+    
+    private int currentLives;
+
+    void Start() { currentLives = maxLives; }
 
     public void DamagePlayer()
     {
         currentLives--;
         lifeUI.sprite = lifeIcons[currentLives];
+        hurtSfx?.Play();
         if (currentLives <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
