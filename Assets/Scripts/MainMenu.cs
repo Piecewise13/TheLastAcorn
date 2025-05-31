398cf7da81274bbba964c9312b9ec7753a6bedd9
Mateo Larrea <mlarreaf99@gmail.com>
2025-05-30 23:12:32 -0700

    removed music in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private float waitTime = 2f;
-    [SerializeField] private AudioPlayer music;
     [SerializeField] private AudioPlayer ambience;
 
     public void LoadGame()
@@ -21,9 +20,5 @@ public class MainMenu : MonoBehaviour
         SceneLoader.LoadNext();
     }
 
-    private void FadeAudio()
-    {
-        music.FadeOut(1f);
-        ambience.FadeOut(1f);
-    }
+    private void FadeAudio() { ambience.FadeOut(1f); }
 }
